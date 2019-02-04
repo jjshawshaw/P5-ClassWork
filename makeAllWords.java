c7157995e17c0df3e3226a8d51df468e5dc27a29
jjshawshaw <jshaw00@stuy.edu>
2019-02-04 11:35:03 -0500

    wrote makeAllWords method

--- makeAllWords.java
+++ makeAllWords.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 public class makeAllWords{
   public static List<String> makeAllWords(int k, int maxLetter){
     ArrayList<String> ans = new ArrayList<String>();
+    makeAllWords(k, maxLetter, "", ans);
     return ans;
+  }
+  private static void makeAllWords(int k, int maxLetter, String s, List<String> ans){
+    if (k == 0){
+      ans.add(s);
+    } else {
+      for (int i = 0; i < maxLetter; i++){
+        makeAllWords(k - 1, maxLetter, (char)('a' + 1) + s, ans);
+      }
+    }
   }
 }
