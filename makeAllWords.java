533d4903f2a2582cbfa25bb06bd93a8e3e3acd32
jjshawshaw <jshaw00@stuy.edu>
2019-02-04 11:30:54 -0500

    wrote empty method

---
+++ makeAllWords.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class makeAllWords{
+  public static List<String> makeAllWords(int k, int maxLetter){
+    ArrayList<String> ans = new ArrayList<String>();
+    return ans;
+  }
+}
